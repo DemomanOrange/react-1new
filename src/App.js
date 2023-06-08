edfe8e5cb8f967fb311f393a53ad47091edf9452
DemomanOrange <ewoniajbecov@gmail.com>
2023-06-08 18:16:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Home"
+import Don from "./Don"
+import { useState } from "react"
+function App(){
+    const [num, setNum] = useState(0)
+    const [don, setDon] = useState('')
+    return (
+        <div className="App">
+        <h1>Hello world!</h1>
+        <Don />
+        <Home />
+        <h1>{num} {num === 15 ? '- u reached max symbols!!!' : num === 0 ? '- u have to type anything here!!!' : 
+        num === 10 ? '- CHOO TAAAM' : ''}</h1>
+        <button onClick={()=>{
+           if(num < 15) setNum(num + 1)
+        }}>+</button>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<button onClick={()=>{
+          if(num > 0)  setNum(num - 1)
+
+        }}>-</button>
+
+
+<button onClick={()=>{
+            setNum(num * 2)
+        }}>*</button>
+
+<button onClick={()=>{
+            setNum(num / 2)
+        }}>/</button>
+
+<button onClick={()=>{
+            setNum(num * 0)
+        }}>reset</button>
+
+<button onClick={()=>{
+            setNum(15)
+        }}>max</button>
+
+<button onClick={()=>{
+          if(num < 15)  setNum(num + 5)
+        }}>+5</button>
+
+<button onClick={()=>{
+         if(num > 0)   setNum(num - 5)
+        }}>-5</button>
+
+
+
+
+        </div>
+
+        
+
+    )
 }
 
-export default App;
+
+export default App
+
+
+
